e7b68c2a52fcf704ab4cdfd6fcaf112e6ca38434
Avinash Soni <avinashsoni.aiml@gmail.comm>
2026-05-09 10:55:04 +0530

    Update URL

--- Backend/src/main/java/com/VibePass/ticket/config/SecurityConfig.java
+++ Backend/src/main/java/com/VibePass/ticket/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         "http://localhost:5173",
-                        "https://vibe-pass-gold.vercel.app/"
+                        "https://vibe-pass-gold.vercel.app"
                 )
         );
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
