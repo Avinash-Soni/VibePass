38a75327076ad0758275e2d91d8f8389f9e574b5
Avinash Soni <avinashsoni.aiml@gmail.comm>
2026-05-09 09:32:48 +0530

    Update config

---
+++ fronted/src/config.js
@@ -0,0 +1,4 @@
+// Ensure there are no extra spaces or characters before 'export'
+export const API_BASE_URL = window.location.hostname === "localhost" 
+  ? "http://localhost:8080" 
+  : "https://vibepass-944b.onrender.com";
